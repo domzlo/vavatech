191b1276b369c440fb5e94364dbb9afb6a951834
Domi <zlotowska.dominika@gmail.com>
2016-10-25 12:15:21 +0200

    Project2_3_Całkowite

--- src/com/company/Całkowite.java
+++ src/com/company/Całkowite.java
@@ -6,6 +6,8 @@ public class Całkowite {
         byte a = 120;
         a = (byte) (a + 20);
 //        rzutujemy int na byte
+//        czyli dodawanie w byte dobrnęło do maxymalnej wartości +127 zaczął liczyć od końca, czyli od -127
+//        ALE W JAVIE NIE WYSKAKUJE BLĄD!!!!
         System.out.println(a);
 
         short s = -32000;
@@ -13,14 +15,20 @@ public class Całkowite {
         System.out.println(s);
 
         int i = 2000000000;
-        i = 2 * i;
+        i = (int) (2 * i);
         System.out.println(i);
 
         char c = 50;
+        char eł = 322;
+        System.out.println(eł);
+//        zgodnie z UNICODE :)
         System.out.println(c);
+//        unicode przyporządkuje znak 2 do nr 50, jako, że 2 zajmuje 50. miejsce w Unicodzie
 
-        int j = '9';
+        int j = '9' + '6';
         System.out.println(j);
+//        przypisujemy char do int, nastąpiła konwersja z char na int zgodnie z Unicode
+//        ale w drugą stronę niż w poprzednim przykładzie
     }
 
 }
