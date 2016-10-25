146decadb341896ff1e1ad0a91927d62c5cad22c
Domi <zlotowska.dominika@gmail.com>
2016-10-25 10:08:16 +0200

    Project2

---
+++ src/com/company/Silnia.java
@@ -0,0 +1,41 @@
+package com.company;
+import java.util.Scanner;
+
+public class Silnia {
+
+    public static void main(String[] args) {
+
+
+        System.out.println("Podaj liczbę");
+        Scanner scanner1 = new Scanner(System.in);
+        int liczba = scanner1.nextInt();
+
+        int wynik = silnia(liczba);
+
+        System.out.println(wynik);
+        System.out.println(wynik);
+    }
+
+    public static int silnia(int p) {
+        if (p == 1) {
+            return 1;
+        }
+            else {
+                int wyniks = (silnia(p - 1)) * p;
+                return wyniks;
+            }
+        }
+
+    }
+
+
+//        System.out.println("Podaj liczbę");
+//        Scanner scanner = new Scanner(System.in);
+//        int k = scanner.nextInt();
+//
+//        int wynik = 1;
+//        for (int i = 1; i <= k; i++) {
+//                wynik = wynik * i;
+//        }
+//
+//        Sys
